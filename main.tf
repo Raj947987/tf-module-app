50f29fde68b55bcde87bc3dc5d7ace42fc3ca4a6
Raja Bhushnam <rajabhushanam117@gmail.com>
2023-06-21 00:11:52 +0530

    Include_tasks in to another ansible file

--- main.tf
+++ main.tf
@@ -58,8 +58,8 @@ resource "aws_iam_instance_profile" "instance_profile" {
   name = "${var.component}-${var.env}-ec2-role"
   role = aws_iam_role.role.name
   }
-resource "aws_iam_group_policy_attachment" "policy-attach" {
-  group      = aws_iam_group.group.name
+resource "aws_iam_role_policy_attachment" "policy-attach" {
+  role       = aws_iam_role.role.name
   policy_arn = aws_iam_policy.policy.arn
 }
 
